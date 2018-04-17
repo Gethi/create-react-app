eb8918f9b7bf92bd99c4ba3bfa24545c67730ac4
Gethi <bigard.antoine@gmail.com>
2018-04-17 20:07:50 +0800

    base copy

---
+++ packages/react-scripts/template/src/routeComponents/About/about.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+import './about.css';
+
+export default () => (
+  <div>
+    <h1>About Us</h1>
+    <p>Hello Medium!</p>
+  </div>
+)
---
+++ packages/react-scripts/template/src/routeComponents/Home/home.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { push } from 'react-router-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import './home.css';
+
+type Props = {
+  changePage: () => void
+};
+
+const Home = (props: Props) => (
+  <div>
+    <h1>Home</h1>
+    <p>Welcome home!</p>
+    <button onClick={props.changePage}>Go to about page via redux</button>
+  </div>
+);
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  changePage: () => push('/about-us')
+}, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Home);
